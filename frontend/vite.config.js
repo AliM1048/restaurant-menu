b61683fc425c93b33fb8f293e2f80c645d43db20
AliM1048 <alimustafa10915@gmail.com>
2026-05-08 22:05:37 +0300

    recommit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: process.env.VITE_BACKEND_URL || "http://localhost:5000",
+        target: "https://restaurant-menu-backend.onrender.com/" || process.env.VITE_BACKEND_URL,
         changeOrigin: true,
       },
     },
