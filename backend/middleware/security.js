934dc70513d275688a9cb69a2e8f137027a5eb15
AliM1048 <alimustafa10915@gmail.com>
2026-05-08 22:29:39 +0300

    updated

--- backend/middleware/security.js
+++ backend/middleware/security.js
@@ -3,7 +3,7 @@ const rateLimit = require("express-rate-limit");
 const cors = require("cors");
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "https://restaurant-menu-nine-orcin.vercel.app/",
+  origin: process.env.FRONTEND_URL || "https://restaurant-menu-nine-orcin.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "multipart/form-data"],
   credentials: true,
